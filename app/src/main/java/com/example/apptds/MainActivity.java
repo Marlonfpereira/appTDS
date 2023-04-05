dbf57ac440c4d538d9d90d265220f3e9b1b52808
Marlonfpereira <marlon.fa.pereira@hotmail.com>
2023-04-05 16:16:19 -0300

    App funcional

---
+++ app/src/main/java/com/example/apptds/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.apptds;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText input = findViewById(R.id.textInput);
+        Button botao = findViewById(R.id.button);
+        TextView resultado = findViewById(R.id.resCalc);
+
+        botao.setOnClickListener(view -> {
+            int valor = 1;
+            String entrada = input.getText().toString();
+            try {
+                valor = Integer.parseInt(entrada);
+            } catch (NumberFormatException e) {
+                System.out.println("Entrada inválida");
+            }
+            resultado.setText(Integer.toString(calculaFatorial(valor)));
+        });
+    }
+
+    int calculaFatorial(int valor) {
+        for (int i = valor-1; i > 0; i--) {
+            valor = valor * i;
+        }
+        return valor;
+    }
+}
